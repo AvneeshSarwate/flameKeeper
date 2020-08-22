c19009642ec840b2274f0a218571cd4c256cdb0d
Avneesh Sarwate <avneeshsarwate@gmail.com>
2020-08-21 21:33:57 -0400

    remove auth

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const app = express();
 const router = express.Router();
 const server = app.listen(process.env.PORT || 8000);
 
-const passport = require('passport');
-const Auth0Strategy = require('passport-auth0');
-
 const createError = require('http-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
@@ -53,26 +50,6 @@ dotenv.config();
 
 
 
-// Configure Passport to use Auth0
-const strategy = new Auth0Strategy({
-    domain: process.env.AUTH0_DOMAIN,
-    clientID: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0_CLIENT_SECRET,
-    callbackURL: process.env.AUTH0_CALLBACK_URL || 'http://localhost:8000/callback'
-  },
-  function (accessToken, refreshToken, extraParams, profile, done) {
-    // accessToken is the token to call Auth0 API (not needed in the most cases)
-    // extraParams.id_token has the JSON Web Token
-    // profile has all the information from the user
-    return done(null, profile);
-  }
-);
-
-
-passport.use(strategy);
-
-
-
 app.use(session(sess));
 
 app.use(logger('dev'));
@@ -81,18 +58,6 @@ app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-// You can use this section to keep a smaller payload
-passport.serializeUser(function (user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function (user, done) {
-  done(null, user);
-});
 
 
 const authRouter = require('./routes/auth');
