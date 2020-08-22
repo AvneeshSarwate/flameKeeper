0989876b1c05482ade79ca9908d17eec1c64a8aa
Avneesh Sarwate <avneeshsarwate@gmail.com>
2020-08-21 22:46:49 -0400

    starting to get past composers working with airtable backend

--- routes/main.js
+++ routes/main.js
@@ -61,6 +61,35 @@ router.get('/history', function (req, res, next) {
     });
 });
 
+router.get('/past-composers', function (req, res, next) {
+    let composerID = req.query.composerID;
+    let from = parseInt(req.query.from) || 0; // Default to entire history
+    let to = parseInt(req.query.to) || FAR_FUTURE; // Default to entire history
+    let limit = parseInt(req.query.limit) || Number.MAX_SAFE_INTEGER;
+    let offset = parseInt(req.query.offset) || 0;
+
+    let composers = Composers.composers;
+    let history;
+    if (composerID) history = state.history.filter(h => h.composerID == composerID);
+    else history = state.history; 
+    
+    // Filter history to time window
+    history = history.filter(h => h.timestamp > from && h.timestamp < to);
+
+    // Filter history for limit and offset
+    history = history.slice(offset, Math.min(offset + limit, history.length));
+
+    // Get composer data for those contained in history
+    historyComposerIDs = [ ...new Set(history.map(h => h.composerID)) ];
+    historyComposers = composers.filter(c => historyComposerIDs.includes(c.composerID));
+
+    res.render("past_composers", {
+        history: history,
+        historyComposers: historyComposers,
+        composerInfo: composers
+    });
+});
+
 router.get('/composer', function (req, res, next) {
     let currentAudio = [ ...state.currentState.audio ];
     currentAudio = currentAudio.map(a => {
