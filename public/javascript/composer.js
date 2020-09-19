fb3a9575aa02d5f021dc67ac4a594f89ce8c1368
Avneesh Sarwate <avneeshsarwate@gmail.com>
2020-09-19 13:25:56 -0400

    adding warning before navigating away from upload page

--- public/javascript/composer.js
+++ public/javascript/composer.js
@@ -72,14 +72,18 @@ function download(filename, text) {
     var element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
-  
+
     element.style.display = 'none';
     document.body.appendChild(element);
-  
+
     element.click();
-  
+
     document.body.removeChild(element);
-  }
+}
+
+window.onbeforeunload = function() {
+    return true;
+};
 
 
 function setGradient(speed, angle, colors){
